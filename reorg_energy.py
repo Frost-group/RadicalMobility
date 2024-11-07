09bb15b48504492ac0d6432ebaa7143350c02fbd
joe-harries <josephcharlesharries@gmail.com>
2024-11-07 10:28:59 +0000

    Update reorg_energy.py
    Spin correction!

--- reorg_energy.py
+++ reorg_energy.py
@@ -14,13 +14,13 @@ def calculate_reorganization_energy(neutral_xyz_file, anion_xyz_file, cation_xyz
     conversion_factor = 27.2114
     E_neutral_optimized = calculate_single_molecule_energy(neutral_xyz_file, 0, 2, "neutral_energy.log")
     E_anion_optimized = calculate_single_molecule_energy(anion_xyz_file, -1, 1, "anion_energy.log")
-    E_neutral_in_anion_geom = calculate_single_molecule_energy(anion_xyz_file, 0, 1, "neutral_in_anion_energy.log")
-    E_anion_in_neutral_geom = calculate_single_molecule_energy(neutral_xyz_file, -1, 2, "anion_in_neutral_energy.log")
+    E_neutral_in_anion_geom = calculate_single_molecule_energy(anion_xyz_file, 0, 2, "neutral_in_anion_energy.log")
+    E_anion_in_neutral_geom = calculate_single_molecule_energy(neutral_xyz_file, -1, 1, "anion_in_neutral_energy.log")
     reorg_energy_electron_hartree = (E_neutral_in_anion_geom - E_neutral_optimized) + (E_anion_in_neutral_geom - E_anion_optimized)
     reorg_energy_electron_eV = reorg_energy_electron_hartree * conversion_factor
     E_cation_optimized = calculate_single_molecule_energy(cation_xyz_file, 1, 1, "cation_energy.log")
-    E_neutral_in_cation_geom = calculate_single_molecule_energy(cation_xyz_file, 0, 1, "neutral_in_cation_energy.log")
-    E_cation_in_neutral_geom = calculate_single_molecule_energy(neutral_xyz_file, 1, 2, "cation_in_neutral_energy.log")
+    E_neutral_in_cation_geom = calculate_single_molecule_energy(cation_xyz_file, 0, 2, "neutral_in_cation_energy.log")
+    E_cation_in_neutral_geom = calculate_single_molecule_energy(neutral_xyz_file, 1, 1, "cation_in_neutral_energy.log")
     reorg_energy_hole_hartree = (E_neutral_in_cation_geom - E_neutral_optimized) + (E_cation_in_neutral_geom - E_cation_optimized)
     reorg_energy_hole_eV = reorg_energy_hole_hartree * conversion_factor
     return reorg_energy_electron_eV, reorg_energy_hole_eV
